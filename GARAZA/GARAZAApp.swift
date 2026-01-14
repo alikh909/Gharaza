e5ec3628c9998d0544838f2a700525ead118c5d0
ali221217 <s2212172438@ku.edu.kw>
2026-01-15 01:35:39 +0300

    Initial Commit

---
+++ GARAZA/GARAZAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GARAZAApp.swift
+//  GARAZA
+//
+//  Created by Ali Alkhulaifi on 15/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct GARAZAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
